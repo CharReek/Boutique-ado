0b952c2a1a5636ed0b3ec4a12833a589a3cc1cc8
CharReek <charlottereek@outlook.com>
2022-07-14 09:53:51 +0000

    deployment part 1

--- botique_ado/settings.py
+++ botique_ado/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -114,8 +115,8 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'botique_ado.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+Database
+https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -123,6 +124,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 
 
 # Password validation
