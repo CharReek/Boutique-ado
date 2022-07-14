b3566760812cb3dd06883a0ae60db48685e8661a
CharReek <charlottereek@outlook.com>
2022-07-14 13:44:12 +0000

    fix spellign error

--- botique_ado/settings.py
+++ botique_ado/settings.py
@@ -177,10 +177,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # cache control 
     AWS_S3_OBJECT_PARAMETERS = {
-        'Ecpires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
     # bucket config
     AWS_STORAGE_BUCKET_NAME = 'django-boutique-ado-creek'
     AWS_S3_REGION_NAME = 'us-east-1'
